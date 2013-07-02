ccd09c7890cf9cf57d7e3aaf3c3e1ecdcdf8fa28
Jeremy Falling <jjfalling@gmail.com>
2013-07-01 20:39:35 -0700

    Added nrpe functionality

--- check_backup_status.sh
+++ check_backup_status.sh
@@ -43,41 +43,99 @@ systemname="backup.domain.com"
 #Where do we want the log to go?
 logfile="/var/log/backup_status_check_log"
 
-
 #############################################################################
+
+#help/usage message
+usage()
+	{
+	cat << usageEOF
+	
+	usage: $0 options
+	Example usage: $0 [-n]
+	
+	This script checks the status of backups. 
+	
+	
+	OPTIONS:
+	-h      Show this message
+	-n      Run as nrpe plugin
+
+	
+usageEOF
+}
+#end help 
+
+
+#this sends the email alert when something goes wrong
+function create_alert {
+
+        echo "$error_message" | mail -s "Backup problem detected during verification on $systemname" "$erroremailaddr"
+
+}
+
+function nrpe_check {
+
+	#see if the logfile is over 25hrs old
+	fileAge=`find $logfile -mmin -1550 |wc -l |tr -d ' '`
+
+	#check if the file is newer then 25hrs
+	if [ "$fileAge" == 1 ]
+	then
+
+			#file is newer then 25hrs, check if things are ok
+			status=`grep -q "Everything seems fine..." $logfile; echo $?`
+			if [ "$status" == 0 ]
+			then
+					echo "Backups are ok"
+					exit 0
+
+
+			#things appear not to be ok
+			else
+					echo "Backups seem to be failing, please check $logfile"
+					exit 2
+
+			fi
+
+
+	#file is older then 25hrs
+	else
+			echo "Backup checker has not ran in the last 25 hrs or file does not exist"
+			exit 2
+
+	fi
+
+}
+
+
+#getops stuff
+while getopts “ht:n” OPTION
+do
+      case $OPTION in
+          h)
+              usage
+              exit 1
+              ;;
+          n)
+              nrpe_check
+              ;;
+          ?)
+              usage
+              exit 1
+              ;;
+      esac
+done
+
+
+
+#get date
+#min_last_run=`date -v -1d +%Y%m%d`
+min_last_run=`date --date="yesterday" +%Y%m%d`
+max_last_run=`date +%Y%m%d`
 
 #delete old log file, and create new one.
 rm $logfile
 touch $logfile
-
-
-unamestr=`uname`
-
-#check os, and use the correct date flags
-if [[ "$unamestr" == 'Linux' ]]; then
-	echo "Looks like OS is Linux" >> $logfile
-	min_last_run=`date --date="yesterday" +%Y%m%d`
-	max_last_run=`date +%Y%m%d`
-   
-elif [[ "$unamestr" == 'FreeBSD' ]]; then
-	echo "Looks like os is FreeBSD" >> $logfile
-	min_last_run=`date -v -1d +%Y%m%d`
-	max_last_run=`date +%Y%m%d`
-   
-else
-	echo "***Not an expected OS... Assuming Linux...***" >> $logfile
-	min_last_run=`date --date="yesterday" +%Y%m%d`
-	max_last_run=`date +%Y%m%d`
-   
-fi
-
-
-#this sends the email alert when something goes wrong
-function send_alert {
-
-	echo "$error_message" | mail -s "Backup problem detected during verification on $systemname" "$erroremailaddr"
-
-}
 
 
 echo "Starting to check backup status" >> $logfile
@@ -86,7 +144,7 @@ echo "" >> $logfile
 
 
 #create array of backup status files
-dir_array=(`ls /backup*/*/tmp/backup_script_status`)
+dir_array=(`ls /*backup*/tmp/backup_script_status`)
 num_of_dir=${#dir_array[*]}
 
 
@@ -100,29 +158,34 @@ while [ $i -lt $num_of_dir ]
 do
 
 
-		result=""
-		last_check=""
-		current_file="${dir_array[$i]}"
-		current_val=`cat $current_file`
+                result=""
+                last_check=""
+                current_file="${dir_array[$i]}"
+                current_val=`cat $current_file`
 
 
-		if [ "$current_val" -ge "$min_last_run" ] && [ "$current_val" -le "$max_last_run" ]
-		then
+                if [ "$current_val" -ge "$min_last_run" ] && [ "$current_val" -le "$max_last_run" ]
+                then
+
                 result="pass"
-        
+
         else
+
+
                 result="fail"
                 final_result="fail"
-        
+
         fi
 
 
         if [ "$current_val" -gt "$max_last_run" ]
         then
+
                 future="true"
-        
+
         fi
-        
+
+
         echo $current_file >> $logfile
         echo $current_val >> $logfile
         echo $result >> $logfile
@@ -138,22 +201,25 @@ loginfo=`cat $logfile`
 
 if [ "$final_result" == "fail" ] &&  [ "$future" == "false" ]
 then
-		error_message="It seems one or more servers have not backed up in the last 24 hours. Please see $logfile"
-		send_alert
 
-		echo "" >> $logfile
-		echo "Alert sent due to one or more servers not backing up" >> $logfile
+                error_message="It seems one or more servers have not backed up in the last 24 hours. Please see $logfile"
+                create_alert
+
+                echo "" >> $logfile
+                echo "Alert sent due to one or more servers not backing up" >> $logfile
 
 
 elif [ "$future" == "true" ]
 then
-		error_message="It seems one or more servers have not backed up in the last 24 hours and atleast one of them is reporting a date in the future. Please  see $logfile"
-		send_alert
-		echo "Alert sent due to one or more server not backing up and one of them reporting time in the future" >> $logfile
+
+                error_message="It seems one or more servers have not backed up in the last 24 hours and atleast one of them is reporting a date in the future. Please  see $logfile"
+                create_alert
+                echo "Alert sent due to one or more server not backing up and one of them reporting time in the future" >> $logfile
 
 else
-		echo "" >> $logfile
-		echo "Everything seems fine..." >> $logfile
+
+                echo "" >> $logfile
+                echo "Everything seems fine..." >> $logfile
 
 fi
 
